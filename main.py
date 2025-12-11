dadbc682b81f30e259b0142bd341720f56e003fc
lishanlung <cnjlwitter@gmail.com>
2025-12-11 01:44:49 +0000

    implemented switch page method + set home page as default

--- main.py
+++ main.py
@@ -14,8 +14,13 @@ class App(ctk.CTk):
         self.sidebar = dash.Sidebar(self, self.pages, self.menu, self.switch_page)
         self.sidebar.pack(side="left", fill="y")
     
-    def switch_page(builder):
+    def switch_page(self, name, builder):
         print("build this page please :3", builder)
+        self.sidebar.set_current_page(name)
 
 app = App(pages=dash.PAGES, menu=dash.MENU)
+#get + set home page by default
+home_page = dash.PAGES.get("HOME")
+app.switch_page(home_page.name, home_page.builder)
+
 app.mainloop()
