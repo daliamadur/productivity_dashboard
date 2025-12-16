8780961b5e21ae4dec6b6780306ae196fdc19848
lishanlung <cnjlwitter@gmail.com>
2025-12-16 16:37:40 +0000

    updated imports

--- productivity_dashboard/__init__.py
+++ productivity_dashboard/__init__.py
@@ -2,3 +2,4 @@ from .assets import HoverImage, load_icon, change_icon_color
 from .appstate import AppState
 from .tabs import MENU, PAGES
 from .sidebar import Sidebar
+from .animations import grow
