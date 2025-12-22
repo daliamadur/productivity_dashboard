d737058f4c33a2c73699e8e7104f197ee7361d1c
lishanlung <cnjlwitter@gmail.com>
2025-12-22 19:38:15 +0000

    update imports

--- productivity_dashboard/__init__.py
+++ productivity_dashboard/__init__.py
@@ -3,6 +3,9 @@ from .appstate import AppState
 from .tabs import MENU, PAGES
 from .sidebar import Sidebar
 from .animations import grow
+from .controllers import *
+from .models import *
+from .storage import *
 
 #for testing purposes
 from .pages.home import HomeTab as CurrentPage
