e3d2ad4b48e1c1dae7cdecac7e01d608372d8b9b
lishanlung <cnjlwitter@gmail.com>
2025-12-22 19:40:54 +0000

    configure appstate skeleton

--- productivity_dashboard/appstate.py
+++ productivity_dashboard/appstate.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+from productivity_dashboard import PomodoroState, Link, Task, Reminder
+
+@dataclass
 class AppState():
-    def __init__(self):
-        return
+    #quick acess links (top n (however many fit))
+    links: list[Link]
+    #pomodoro time -> time left, whether it's a break or work, pomodoro status (running or paused)
+    pomodoro: PomodoroState
+    #next reminder -> name, when it's due maybe?
+    next_reminder: Reminder
+    #top tasks -> (again top n highest priority)
+    top_tasks = list[Task]
